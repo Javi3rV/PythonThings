348843d01e8dbb18e0ad694f1f0cd5f5f2718e4a
Javi3rV <javirubik22@gmail.com>
2023-04-18 14:55:26 +0200

    implemented new features from advanced branch

--- emailVer.py
+++ emailVer.py
@@ -4,15 +4,26 @@ for letter in email:
     if letter == "@":
         contArroba += 1
 
-
-if contArroba == 1:
-    print("El email parece correcto")
-else:
-    print("El email no parece correcto")
-
 def emailDomain(email):
     domain = ""
     for index, letter in email:
         if letter == "@":
             domain = email[index+1:]
     return domain
+def domainTLD(domain):
+    tld = "err"
+    if domain.count(".")==1:
+        for index, letter in domain:
+            if letter == ".":
+                if len(domain[index+1:]) in range(2,4):
+                    tld = domain[index+1:]
+
+    return tld
+
+
+
+if contArroba == 1 and domainTLD(emailDomain(email)) != "err":
+    print("El email parece correcto")
+else:
+    print("El email no parece correcto")
+
