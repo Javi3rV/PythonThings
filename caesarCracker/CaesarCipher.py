3470cdc016c62343c018f42916c516ce8554a689
Javi3rV <javirubik22@gmail.com>
2023-05-05 21:53:42 +0200

    added CaesarCracker.py

---
+++ caesarCracker/CaesarCipher.py
@@ -0,0 +1,36 @@
+dict = "abcdefghijklmnñopqrstuvwxyz"
+
+def crypt(text, rots):
+    text = list(text.lower())
+    for index, letter in enumerate(text):
+        if letter in dict:
+            originalIndex = dict.index(letter)
+            cipheredIndex = (originalIndex+rots) % len(dict)
+            text[index] = dict[cipheredIndex]
+    return "".join(text)
+
+def decrypt(text, rots):
+    text = list(text)
+    for index, letter in enumerate(text):
+        if letter in dict:
+            cipheredIndex = dict.index(letter)
+            originalIndex = cipheredIndex-rots
+            text[index] = dict[originalIndex]
+    return "".join(text)
+
+if __name__ == "__main__":
+    opt = input("Encrypt or Decrypt? (E/D): ")
+    if opt.lower() == "e":
+        try:
+            text = input("Input text to encrypt: ")
+            rots = int(input("Number of rotations: "))
+        except:
+            print("Something went wrong, please enter data correctly")
+        print(crypt(text, rots))
+    elif opt.lower() == "d":
+        try:
+            text = input("Input text to decrypt: ")
+            rots = int(input("Number of rotations: "))
+        except:
+            print("Something went wrong, please enter data correctly")
+        print(decrypt(text, rots))
---
+++ caesarCracker/CaesarCracker.py
@@ -0,0 +1,54 @@
+import CaesarCipher as cCrypt
+dict = "abcdefghijklmnñopqrstuvwxyz"
+englishDict = open("caesarCracker/englishDict.txt", "r", encoding="utf-8", errors="ignore").read().replace("\n", " ").split()
+spanishDict = open("caesarCracker/spanishDict.txt", "r", encoding="utf-8").read().replace("\n", " ").split()
+text = input("Text to crack: ")
+found = 0
+for key in range(len(dict)):
+    testResult = cCrypt.decrypt(text, key)
+
+    tokens = []
+    token =""
+    if len(testResult)<4:
+        limit = 2
+    else:
+        limit = 4
+    for index, letter in enumerate(testResult):
+        token += letter
+        if len(token)==limit:
+            tokens.append(token)
+            token = ""
+        if len("".join(tokens))+limit > len(testResult):
+            tokens.append(testResult[index:])
+            break
+    
+
+    tokenCount = 0
+    for token in tokens: 
+        if "a".join(englishDict).count(token.strip())>3 or "a".join(spanishDict).count(token.strip())>3:
+            tokenCount += 1
+    if tokenCount > 2:
+        print("\nPossible result found! -> Key: {0} rotations - Result: {1}\n".format(key, testResult))
+        found += 1
+    else:
+        print("test {0} rotations - result: {1}".format(key,testResult))
+if found == 0:
+    print("\n---------Nothing apparently found, retrying-----------\n")
+    for key in range(len(dict)):
+        testResult = cCrypt.decrypt(text, key)
+
+        tokens = testResult.split()
+            
+
+        tokenCount = 0
+        for token in tokens: 
+            if len(token)<3:
+                continue
+            if "a".join(englishDict).count(token.strip())>3 or "a".join(spanishDict).count(token.strip())>3:
+                tokenCount += 1
+        if tokenCount > 0:
+            print("\nPossible result found! -> Key: {0} rotations - Result: {1}\n".format(key, testResult))
+            found += 1
+        else:
+            print("test {0} rotations - result: {1}".format(key,testResult))
+
